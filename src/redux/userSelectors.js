f2d943d33d20e4a10f4ce546104e0d5ff6c0bb2e
unknown <r2598509@gmail.com>
2020-07-09 15:28:39 +0300

    connect reselect

--- src/redux/userSelectors.js
+++ src/redux/userSelectors.js
@@ -1,6 +1,15 @@
-export const getUsers = (state) => {
+import { createSelector } from 'reselect';
+
+const getUsersSelector = (state) => {
     return state.usersPage.users;
 }
+
+export const getUsers = createSelector(
+    [getUsersSelector],
+    (users) => {
+        return users.filter(() => true); // for test
+    }
+);
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
