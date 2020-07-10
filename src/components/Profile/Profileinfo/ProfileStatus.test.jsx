c22b0f588d4ac2629361025cbb0d41dd7067069e
unknown <r2598509@gmail.com>
2020-07-10 21:11:21 +0300

    test-driven development, jest, mock / ProfileStatus component

--- src/components/Profile/Profileinfo/ProfileStatus.test.jsx
+++ src/components/Profile/Profileinfo/ProfileStatus.test.jsx
@@ -6,6 +6,7 @@ describe("ProfileStatus component", () => {
     test("status from props should be in the state", () => {
       const component = create(<ProfileStatus status="my status" />);
       const instance = component.getInstance();
+
       expect(instance.state.status).toBe("my status");
     });
 
@@ -13,13 +14,53 @@ describe("ProfileStatus component", () => {
       const component = create(<ProfileStatus status="my status" />);
       const instance = component.root;
       const span = instance.findByType("span");
-      expect(span.props.children).toBe("my status");;
+
+      expect(span.props.children).toBe("my status");
+    });
+
+    test("input shouldn't be displayed after creation", () => {
+      const component = create(<ProfileStatus status="my status" />);
+      const instance = component.root;
+      
+      expect(() => {
+        instance.findByType("input");
+      }).toThrow();
     });
 
     test("span should be displayed after creation", () => {
       const component = create(<ProfileStatus status="my status" />);
       const instance = component.root;
       const span = instance.findByType("span");
-      expect(span).not.toBeNull();;
+
+      expect(span).not.toBeNull();
+    });
+
+    test("span should be displayed after creation", () => {
+      const component = create(<ProfileStatus status="my status" />);
+      const instance = component.root;
+      const span = instance.findByType("span");
+
+      expect(span).not.toBeNull();
+    });
+
+    test("input should be displayed in edit mode instead span", () => {
+      const component = create(<ProfileStatus status="my status" />);
+      const instance = component.root;
+      const span = instance.findByType("span");
+
+      span.props.onDoubleClick();
+      
+      const input = instance.findByType("input");
+
+      expect(input.props.value).toBe("my status");
+    });
+
+    test("callback should be called", () => {
+      const mockCallback = jest.fn();
+      let component = create(<ProfileStatus status="my status" updateStatus={mockCallback} />);
+      const instance = component.getInstance();
+      instance.deactivateEditMode();
+      
+      expect(mockCallback.mock.calls.length).toBe(1);
     });
   });
