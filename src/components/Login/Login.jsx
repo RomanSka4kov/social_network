5ed5e9dd8db82233fb7ef58826ef666cf0b65708
unknown <r2598509@gmail.com>
2020-07-12 20:26:19 +0300

    add Captcha to Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,11 +7,17 @@ import { required } from '../../utils/validators/validators';
 import { Redirect } from 'react-router-dom';
 import style from './../common/FormsControls/FormsControls.module.css';
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return <form onSubmit={handleSubmit}>
         {createField("Email", "email", Input, [required])}
         {createField("Password", "password", Input, [required], {type: "password"})}
         {createField(null, "rememberMe", Input, [], {type: "checkbox"}, " remember me")}
+        { captchaUrl &&
+            <div>
+                <img src={captchaUrl} alt="captcha" />
+                {createField("Type symbols from img", "captcha", Input, [required])}
+            </div>
+        }
         { error && 
             <div className={style.formSummaryError}>
                 <span>Login or email invalid</span>
@@ -25,9 +31,9 @@ const LoginForm = ({handleSubmit, error}) => {
 
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
-const Login = ({login, isAuth}) => {
+const Login = ({login, isAuth, captchaUrl}) => {
     const onSubmit = (formData) => {
-        login(formData.email, formData.password, formData.rememberMe);
+        login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
 
     if (isAuth) {
@@ -36,12 +42,13 @@ const Login = ({login, isAuth}) => {
 
     return <div>
         <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit} />
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={captchaUrl} />
     </div>
 }
 
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth
+    isAuth: state.auth.isAuth,
+    captchaUrl: state.auth.captchaUrl
 })
 
 export default connect(mapStateToProps, {login})(Login);
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,19 +1,25 @@
-import { authAPI } from "../api/api";
+import { authAPI, securityAPI } from "../api/api";
 import { stopSubmit } from "redux-form";
 
 const SET_USER_DATA = 'auth/SET_USER_DATA';
+const GET_CAPTCHA_URL_SUCCESS = 'auth/GET_CAPTCHA_URL_SUCCESS';
 
 
 let initialState = {
     userId: null,
     email: null,
     login: null,
-    isAuth: false
+    isAuth: false,
+    captchaUrl: null // if not null captcha will displayed on UI
 }
 
 const authReducer = (state = initialState, action) => {
     switch(action.type) {
         case SET_USER_DATA:
+            return {
+                ...state, ...action.payload
+            }
+        case GET_CAPTCHA_URL_SUCCESS:
             return {
                 ...state, ...action.payload
             }
@@ -23,8 +29,10 @@ const authReducer = (state = initialState, action) => {
 
 export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}});
 
+export const getCaptchaUrlSuccess = (captchaUrl) => ({type: GET_CAPTCHA_URL_SUCCESS, payload: {captchaUrl}});
+
 export const getAuthUserData = () => async (dispatch) => {
-    let response = await authAPI.me();
+    const response = await authAPI.me();
 
     if (response.data.resultCode === 0) {
         let {id, login, email} = response.data.data;
@@ -32,20 +40,29 @@ export const getAuthUserData = () => async (dispatch) => {
     }
 }
 
-export const login = (email, password, rememberMe) => async (dispatch) => {
-    let response = await authAPI.login(email, password, rememberMe);
+export const login = (email, password, rememberMe, captcha) => async (dispatch) => {
+    const response = await authAPI.login(email, password, rememberMe, captcha);
 
-    if (response.data.resultCode === 0) {
+    if (response.data.resultCode === 0) { // success
         dispatch(getAuthUserData());
-    // if (response.data.resultCode === 10) - captcha
-    } else {
-        let errorMessage = response.data.message > 0 ? response.data.message[0] : 'invalid login or email';
+    } else { // error
+        if (response.data.resultCode === 10) { // captcha
+            dispatch(getCaptchaUrl());
+        }
+        let errorMessage = response.data.messages > 0 ? response.data.messages[0] : 'invalid login or email';
         dispatch(stopSubmit('login', {_error: errorMessage}));
     }
 }
 
+
+export const getCaptchaUrl = () => async (dispatch) => {
+    const response = await securityAPI.getCaptchaUrl();
+    const capchaUrl = response.data.url;
+    dispatch(getCaptchaUrlSuccess(capchaUrl));
+}
+
 export const logout = () => async (dispatch) => {
-    let response = await authAPI.logout();
+    const response = await authAPI.logout();
     
     if (response.data.resultCode === 0) {
         dispatch(setAuthUserData(null, null, null, false));
